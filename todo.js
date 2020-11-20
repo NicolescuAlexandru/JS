526fe35ec604b07c41daa7301d99b7490d00e961
Nicolescu Alexandru <74272723+NicolescuAlexandru@users.noreply.github.com>
2020-11-20 08:24:24 +0200

    Add files via upload
    A very simple to do list with JS and browser prompt

---
+++ todo.js
@@ -0,0 +1,27 @@
+let input = prompt('What would you like to add?');
+const todos = [];
+
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log('-----------------------');
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`)
+        }
+        console.log('-----------------------');
+    } else if (input === 'new') {
+        const newTodo = prompt('What is the new todo entry?')
+        todos.push(newTodo);
+        console.log(`The entry ${newTodo} was added successfully.`)
+    } else if (input === 'delete') {
+        const toDelete = parseInt(prompt('What is the index of the entry to be deleted?'));
+        if (!Number.isNaN(toDelete) && toDelete < todos.length) {
+            todos.splice(toDelete, 1);
+            console.log(`The entry indexed ${toDelete} was deleted successfully.`);
+        } else {
+            console.log('Please enter a valid index!');
+        }
+    }
+    input = prompt('What would you like to add?');
+}
+
+console.log('Ok, the app is quit now.');
